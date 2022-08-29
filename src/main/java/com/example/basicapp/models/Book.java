de3c0a8dea47f97f80f5b35d4bb5d3afbc75b415
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-08-29 17:10:01 -0500

    adding first entity

---
+++ src/main/java/com/example/basicapp/models/Book.java
@@ -0,0 +1,19 @@
+package com.example.basicapp.models;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+}
