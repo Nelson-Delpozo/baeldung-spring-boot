cb941f6d44d62a3ff426bb4c84a2abcaf8b08336
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-09-06 18:35:25 -0500

    fixed app class

--- src/main/java/com/example/basicapp/BasicAppApplication.java
+++ src/main/java/com/example/basicapp/BasicAppApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("com.example.basicapp")
-@EntityScan("com.example.basicapp")
+@EnableJpaRepositories("com.example.basicapp.repositories")
+@EntityScan("com.example.basicapp.models")
 @SpringBootApplication
 public class BasicAppApplication {
 
